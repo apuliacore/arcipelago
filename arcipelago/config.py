75604932110abca694eb3dbe14e8b7ba9db53314
Flavio <flavio.petruzzellis@gmail.com>
2023-04-15 23:00:08 +0200

    Add config to repo

---
+++ arcipelago/config.py
@@ -0,0 +1,12 @@
+# Telegram
+chatbot_token = '0'
+main_channel = 0
+notification_channel = 0
+main_group = 0
+authorized_users = { 0: 'Pippo' }
+daily_update_interval = 0
+
+# Optional
+# Google Calendar
+SERVICE_ACCOUNT_FILE = ""
+CALENDAR_ID = ""
