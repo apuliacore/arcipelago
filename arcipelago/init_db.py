e0f53972482cca020c6047588e454589b4d41675
Flavio <flavio.petruzzellis@gmail.com>
2023-05-18 13:04:15 +0200

    Create poster folder at installation

--- arcipelago/init_db.py
+++ arcipelago/init_db.py
@@ -1,10 +1,19 @@
 from arcipelago.db import init_db
 from arcipelago.config import development
+import os
+
+
+def init_poster_folder():
+	poster_folder_name = "./locandine/"
+	if not os.path.exists(poster_folder_name):
+		os.makedirs(poster_folder_name)
+		print("Initialized posters folder.")
 
 
 def main():
 	init_db(development)
 	print("Initialized database.")
+	init_poster_folder()
 
 
 if __name__ == '__main__':
