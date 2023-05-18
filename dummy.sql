75692476b004412fef8e9f3e98d7521e59a02729
Flavio <flavio.petruzzellis@gmail.com>
2023-05-18 12:51:25 +0200

    Add dummy test data

---
+++ dummy.sql
@@ -0,0 +1,57 @@
+INSERT INTO event (name, venue, verified_venue_id,
+				   start_datetime, end_datetime, description, confirmed,
+				   published, price, categories)
+VALUES ("Festa di Fine Estate",
+	"Spazio13, Bari",
+	null,
+	'2022-10-10 15:00:00',
+	'2022-10-10 18:00:00',
+	"FESTA DI FINE ESTATE 🏖️
+Il centro estivo PiratiCulturali22 è terminato il mese scorso portando a casa sorrisi, abbracci e tanto tanto priscio 😍
+
+Per questo abbiamo deciso di celebrarne le gesta con:
+👇👇👇
+
+🎲 Giochi da tavolo
+🤖 Lezioni di Robotica
+📖 Letture per bambini con Gigilegge & il Sindaco di Madonnella
+🧙 Magia e mistero con Mago Arolf Prestigiatore Bari
+
+Un'occasione per incontrarci e discutere dei progetti futuri dei nostri piccoli PiratiCulturali 🏴‍☠️",
+	True,
+	False,
+	0,
+	'fumetti e giochi');
+
+INSERT INTO event (name, venue, verified_venue_id,
+				   start_datetime, end_datetime, description, confirmed,
+				   published, price, categories)
+VALUES ("Corso di Ballo FIFTIES JIVE",
+	"MAT Laboratorio Urbano, Terlizzi",
+	null,
+	'2022-09-11 20:00:00',
+	null,
+	"Amanti degli anni 50 Nicola e Giada sono lieti di presentarvi, per la prima volta in Puglia, lunedì 3 ottobre ore 20 presso la sede MAT laboratorio urbano Terlizzzi, una lezione prova dello stile di ballo FIFTIES JIVE.
+
+Per maggiori info e prenotazioni scrivere al numero 12345678",
+	False,
+	False,
+	0,
+	'musica, danza');
+
+INSERT INTO event (name, venue, verified_venue_id,
+				   start_datetime, end_datetime, description, confirmed,
+				   published, price, categories)
+VALUES ("PhEST 2022 | Opening Days",
+		"Largo Palmieri, 70043 Monopoli BA, Italia",
+		null,
+		'2022-09-09 20:00:00',
+		'2022-09-11 23:59:59',
+		"Si inizia il 9 settembre con l'apertura speciale di Palazzo Palmieri, musica con dj set di Populous alle 22 e vino offerto da Tormaresca.
+Si continua il 10 e 11 settembre con visite guidate, un ciclo di talk organizzato con l'Università degli Studi Aldo Moro di Bari, letture portfolio, laboratori e proiezioni nelle giornate.
+
+Tutto il programma sul sito dell'evento: www.phest.info",
+		False,
+		False,
+		0,
+		'fotografia');
